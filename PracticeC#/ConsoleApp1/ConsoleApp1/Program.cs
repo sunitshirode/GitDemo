55c7c6e7adb8ec7d25187be60c45b9fa0467fca0
Shirode, Sunit <sunitshirode@gmail.com>
2021-03-12 21:49:47 +0530

    C# projects added.

---
+++ PracticeC#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        String name;
+        int rollNo;
+        int age;
+        int division;
+
+        void printStudent()
+        {
+            Console.WriteLine("Name of Student:\t\t" + name);
+            Console.WriteLine("Roll Number of Student:\t\t" + rollNo);
+            Console.WriteLine("Age of Student:\t\t\t" + age);
+            Console.WriteLine("Division of Student:\t\t" + division);
+        }
+
+        Student(string passedName, int passedRollNo, int passedAge, int passedDivision)
+        {
+            name = passedName;
+            rollNo = passedRollNo;
+            age = passedAge;
+            division = passedDivision;
+            printStudent();
+        }
+
+               static void Main(string[] args)
+        {
+
+            Student sunit = new Student("sunit", 10, 38, 4);
+            Student sameer = new Student("sameer", 11, 35, 5);
+            Student utkarsh = new Student("utkarsh", 10, 36, 6);
+
+
+        }
+    }
+}
---
+++ PracticeC#/Pets/Pets/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+
+
+namespace Pets
+
+{
+
+    class Pets
+
+    {
+        String nameOfPet;
+        String typeOfPet;
+        float weightOfPet;
+
+        void speak()
+        {
+            Console.WriteLine(nameOfPet + "\tcan speak.");
+        }
+
+        void eat()
+        {
+            Console.WriteLine(nameOfPet + "\tcan eat.");
+        }
+        void walk()
+        {
+            Console.WriteLine(nameOfPet + "\tcan walk.");
+        }
+        void fly()
+        {
+            Console.WriteLine(nameOfPet + "\tcan fly.");
+        }
+        Pets(String name)
+        {
+            nameOfPet = name;
+        }
+        static void Main(string[] args)
+        {
+            Pets dog = new Pets("dog");
+
+            Pets cat = new Pets("cat");
+
+            Pets duck = new Pets("duck");
+
+            Pets sparrow = new Pets("sparrow");
+
+            dog.walk();
+            dog.eat();
+
+            cat.walk();
+            cat.eat();
+
+            duck.eat();
+            duck.speak();
+            duck.walk();
+
+            sparrow.fly();
+        }
+    }
+}
+/*create Pets class. 
+ * create some properties: Name, Type, Weight
+ * create some methods: Speak, eat, walk, fly
+ * Create objects of pets - dog, cat, ducks, sparrow
+ * if a dog is speaking, it should print 'dog is speaking'.
+ * similarly for others
+*/
